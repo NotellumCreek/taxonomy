9d8deaf1aff7cad8b8d72ed4ed465f0b56c9da98
NotellumCreek <89566860+NotellumCreek@users.noreply.github.com>
2024-03-07 14:34:20 -0800

    Update layout.tsx
    Check if logged in, if so, show UserAccountNav

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -5,34 +5,51 @@ import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
 import { MainNav } from "@/components/main-nav"
 import { SiteFooter } from "@/components/site-footer"
+import { getCurrentUser } from "@/lib/session"
+import { UserAccountNav } from "@/components/user-account-nav"
 
 interface MarketingLayoutProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export default async function MarketingLayout({
   children,
 }: MarketingLayoutProps) {
+  // Check is user is logged in
+  const user = await getCurrentUser();
+
   return (
     <div className="flex min-h-screen flex-col">
       <header className="container z-40 bg-background">
         <div className="flex h-20 items-center justify-between py-6">
           <MainNav items={marketingConfig.mainNav} />
           <nav>
-            <Link
-              href="/login"
-              className={cn(
-                buttonVariants({ variant: "secondary", size: "sm" }),
-                "px-4"
-              )}
-            >
-              Login
-            </Link>
+            {user ? (
+              // Render UserAccountNav if user is logged in
+              <UserAccountNav
+                user={{
+                  name: user.name,
+                  image: user.image,
+                  email: user.email,
+                }}
+              />
+            ) : (
+              // Render login button if user is not logged in
+              <Link
+                href="/login"
+                className={cn(
+                  buttonVariants({ variant: "secondary", size: "sm" }),
+                  "px-4"
+                )}
+              >
+                Login
+              </Link>
+            )}
           </nav>
         </div>
       </header>
       <main className="flex-1">{children}</main>
       <SiteFooter />
     </div>
-  )
+  );
 }
